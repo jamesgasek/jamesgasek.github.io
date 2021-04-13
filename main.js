ec680686744c0a904c4f3d0b72de32554484ff8e
James Gasek <techie1461@gmail.com>
2021-04-13 13:46:23 -0400

    added some js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-main
+function main() {
+    "use strict";
+    var counter = 0, all = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const myname = "James Gasek"
+    for (var i = 0; i < 11; i++) {
+        setTimeout(function () {
+            console.log(counter)
+            var text = document.getElementById("matrix");
+            text.innerHTML = text.innerHTML + myname.charAt(counter)
+            counter = (counter+1);
+        }, i * 60)
+    }
+}
+window.onload = main;

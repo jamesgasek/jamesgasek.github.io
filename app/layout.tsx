f2918961384e558d0d8725b42ca642e8a25057cb
James Gasek <jamesgasek@Jamess-MacBook-Air.local>
2024-08-12 13:03:09 -0400

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import { Navbar } from './components/nav';
+// import { Head } from 'next/document';
 // import { SandpackCSS } from './blog/[slug]/sandpack';
 
 export const metadata: Metadata = {
@@ -58,7 +59,8 @@ export default function RootLayout({
       )}
     >
       <head>
-        {/* <SandpackCSS /> */}
+        <script data-goatcounter="https://jamesgasekpage.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js"></script>
       </head>
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">

7727ce82289907b64e5a4d252cb1e3766a8da347
James Gasek <james@gasek.net>
2025-03-27 07:46:46 -0400

    add resume link

--- app/page.tsx
+++ app/page.tsx
@@ -216,6 +216,20 @@ export default function Page() {
             <p className="ml-2 h-7">follow me</p>
           </a>
         </li>
+
+
+      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300"></ul>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://gasek.net/resume"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">resume</p>
+          </a>
+        </li>
       
       </ul>
     </section>
